fbac67f3a75dc24bc2701cd67d6daed324c4bece
UFAivovaurio <1459538260@qq.com>
2024-07-23 13:12:47 +0800

    first

---
+++ .vuepress/config.ts
@@ -0,0 +1,53 @@
+import { defineUserConfig } from "vuepress";
+import recoTheme from "vuepress-theme-reco";
+import { viteBundler } from '@vuepress/bundler-vite'
+import { webpackBundler } from '@vuepress/bundler-webpack'
+
+export default defineUserConfig({
+  title: "UF-OuO",
+  description: "Just playing around",
+  bundler: viteBundler(),
+  // bundler: webpackBundler(),
+  theme: recoTheme({
+    style: "@vuepress-reco/style-default",
+    logo: "/logo.png",
+    author: "UF-OVO",
+    authorAvatar: "/head.png",
+    docsRepo: "https://github.com/UFAivovaurio",
+    docsBranch: "main",
+    docsDir: "example",
+    lastUpdatedText: "",
+    // series 为原 sidebar
+    series: {
+      "/docs/theme-reco/": [
+        {
+          text: "module one",
+          children: ["home", "theme"],
+        },
+        {
+          text: "module two",
+          children: ["api", "plugin"],
+        },
+      ],
+    },
+    navbar: [
+      { text: "主页", link: "/" },
+      { text: "目录", link: "/categories/reco/1/" },
+      { text: "标签", link: "/tags/tag1/1/" }
+    ],
+    // commentConfig: {
+    //   type: 'valine',
+    //   // options 与 1.x 的 valineConfig 配置一致
+    //   options: {
+    //     // appId: 'xxx',
+    //     // appKey: 'xxx',
+    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
+    //     // verify: true, // 验证码服务
+    //     // notify: true,
+    //     // recordIP: true,
+    //     // hideComments: true // 隐藏评论
+    //   },
+    // },
+  }),
+  // debug: true,
+});
---
+++ README.md
@@ -0,0 +1,24 @@
+---
+home: true
+modules:
+  - BannerBrand
+  - Blog
+  - MdContent
+  - Footer
+bannerBrand:
+  bgImage: '/bg.svg'
+  title: UF-OuO
+  description: 浪花淘尽。
+  tagline: 博客网站
+  buttons:
+    - { text: Guide, link: '/docs/guide/introduce' }
+    - { text: Default Style, link: '/docs/style-default-api/introduce', type: 'plain' }
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/UFAivovaurio' }
+blog:
+  socialLinks:
+    - { icon: 'LogoGithub', link: 'https://github.com/UFAivovaurio' }
+isShowTitleInHome: true
+actionText: About
+actionLink: /views/other/about
+---
