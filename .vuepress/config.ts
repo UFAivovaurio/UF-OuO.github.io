64fe427af8499294f2ff1c8b7ac2bb96ef7f0e0e
UFAivovaurio <1459538260@qq.com>
2024-07-23 13:47:18 +0800

    modify config.js

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -4,6 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
+  base:"/UF-OuO.github.io/",
   title: "UF-OuO",
   description: "Just playing around",
   bundler: viteBundler(),
